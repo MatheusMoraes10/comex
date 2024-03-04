1f6856b915898e161e7a3126d0df158f6d5b36d9
unknown <moraes.compras@hotmail.com>
2024-03-04 09:40:09 -0300

    Upload exercicio 06 (Restringir visibilidade das propriedades do Produto) - Sprint 07

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         Produto produto1 = new Produto();
-        produto1.nome = "Espelho";
-        produto1.descricao = "Moldura cinza";
-        produto1.precoUnitario = 89.90;
-        produto1.quantidade = 10;
+        produto1.setNome("Espelho");
+        produto1.setDescricao("Moldura cinza");
+        produto1.setPrecoUnitario(89.90);
+        produto1.setQuantidade(10);
 
         Produto produto2 = new Produto();
-        produto2.nome = "Espiriteira";
-        produto2.descricao = "Combustível utilizado: Alcool";
-        produto2.precoUnitario = 250.99;
-        produto2.quantidade = 35;
+        produto2.setNome("Espiriteira");
+        produto2.setDescricao("Combustível utilizado: Alcool");
+        produto2.setPrecoUnitario(250.99);
+        produto2.setQuantidade(35);
 
         /*Exercício 2 (Crie Classe do produto)
         System.out.println("nome :" + produto1.nome + "\nDescrição: " + produto1.descricao
@@ -32,32 +32,36 @@ public class Main {
 //       produto2.imprimirDadosProduto();
 
 //      Exercício 05 (Crie as classes Cliente e Endereco)
-        Endereco endereco1 = new Endereco();
-        endereco1.bairro = "Santana";
-        endereco1.cidade = "São Paulo";
-        endereco1.complemento = "Casa de esquina";
-        endereco1.estado = "SP";
-        endereco1.rua = "Braz Leme";
-        endereco1.numero = 1000;
+//        Endereco endereco1 = new Endereco();
+//        endereco1.bairro = "Santana";
+//        endereco1.cidade = "São Paulo";
+//        endereco1.complemento = "Casa de esquina";
+//        endereco1.estado = "SP";
+//        endereco1.rua = "Braz Leme";
+//        endereco1.numero = 1000;
+//
+//        Cliente cliente1 = new Cliente();
+//        cliente1.nome = "Angelo";
+//        cliente1.cpf = "78945612399";
+//        cliente1.email = "alura@alura.com";
+//        cliente1.profissao = "Programador";
+//        cliente1.telefone = "11954481236";
+//        cliente1.endereco = endereco1;
+//
+//        System.out.println(cliente1.nome);
+//        System.out.println(cliente1.cpf);
+//        System.out.println(cliente1.email);
+//        System.out.println(cliente1.profissao);
+//        System.out.println(cliente1.telefone);
+//        System.out.println(cliente1.endereco.bairro);
+//        System.out.println(cliente1.endereco.cidade);
+//        System.out.println(cliente1.endereco.complemento);
+//        System.out.println(cliente1.endereco.estado);
+//        System.out.println(cliente1.endereco.rua);
+//        System.out.println(cliente1.endereco.numero);
 
-        Cliente cliente1 = new Cliente();
-        cliente1.nome = "Angelo";
-        cliente1.cpf = "78945612399";
-        cliente1.email = "alura@alura.com";
-        cliente1.profissao = "Programador";
-        cliente1.telefone = "11954481236";
-        cliente1.endereco = endereco1;
-
-        System.out.println(cliente1.nome);
-        System.out.println(cliente1.cpf);
-        System.out.println(cliente1.email);
-        System.out.println(cliente1.profissao);
-        System.out.println(cliente1.telefone);
-        System.out.println(cliente1.endereco.bairro);
-        System.out.println(cliente1.endereco.cidade);
-        System.out.println(cliente1.endereco.complemento);
-        System.out.println(cliente1.endereco.estado);
-        System.out.println(cliente1.endereco.rua);
-        System.out.println(cliente1.endereco.numero);
+//        Exercício 06 (Restringir visibilidade das propriedades do Produto)
+        System.out.println("nome :" + produto1.getNome() + "\nDescrição: " + produto1.getDescricao()
+                + "\nPreço: " + produto1.getPrecoUnitario() + "\nQuantidade: " + produto1.getQuantidade());
     }
 }
--- src/main/java/br/com/alura/comex/Produto.java
+++ src/main/java/br/com/alura/comex/Produto.java
@@ -1,10 +1,42 @@
 package br.com.alura.comex;
 
 public class Produto {
-    String nome;
-    String descricao;
-    double precoUnitario;
-    int quantidade;
+    private String nome;
+    private String descricao;
+    private double precoUnitario;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 
     public void imprimirDadosProduto () {
 
