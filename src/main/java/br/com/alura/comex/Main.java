6aa457c3d496676b36e30c038e41fb7a77d9f1bc
unknown <moraes.compras@hotmail.com>
2024-03-04 09:07:22 -0300

    Upload exercicio 05 (Crie as classes Cliente e Endereco) - Sprint 07

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -28,7 +28,36 @@ public class Main {
         */
 
         //Exercício 04 (Crie um comportamento de impressão dos dados de um produto)
-        produto1.imprimirDadosProduto();
-        produto2.imprimirDadosProduto();
+//       produto1.imprimirDadosProduto();
+//       produto2.imprimirDadosProduto();
+
+//      Exercício 05 (Crie as classes Cliente e Endereco)
+        Endereco endereco1 = new Endereco();
+        endereco1.bairro = "Santana";
+        endereco1.cidade = "São Paulo";
+        endereco1.complemento = "Casa de esquina";
+        endereco1.estado = "SP";
+        endereco1.rua = "Braz Leme";
+        endereco1.numero = 1000;
+
+        Cliente cliente1 = new Cliente();
+        cliente1.nome = "Angelo";
+        cliente1.cpf = "78945612399";
+        cliente1.email = "alura@alura.com";
+        cliente1.profissao = "Programador";
+        cliente1.telefone = "11954481236";
+        cliente1.endereco = endereco1;
+
+        System.out.println(cliente1.nome);
+        System.out.println(cliente1.cpf);
+        System.out.println(cliente1.email);
+        System.out.println(cliente1.profissao);
+        System.out.println(cliente1.telefone);
+        System.out.println(cliente1.endereco.bairro);
+        System.out.println(cliente1.endereco.cidade);
+        System.out.println(cliente1.endereco.complemento);
+        System.out.println(cliente1.endereco.estado);
+        System.out.println(cliente1.endereco.rua);
+        System.out.println(cliente1.endereco.numero);
     }
 }
